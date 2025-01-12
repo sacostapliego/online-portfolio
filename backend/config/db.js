c295999b5519fdf8975e874dcc74b6846b334a3e
Steven <steven.acosta867@gmail.com>
2025-01-12 12:30:56 -0500

    Deployment Update 4

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,12 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const mongoUri = secret.MONGO_URI; // Use the secret fetched above
+    const conn = await mongoose.connect(mongoUri);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(1); // process code 1 means exit with failure, 0 means exit with success
+    process.exit(1);
   }
-}
+};
