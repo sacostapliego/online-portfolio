84ed5cc59487842363b89adfa634e09cf98ad34f
Steven <steven.acosta867@gmail.com>
2025-01-12 12:51:19 -0500

    Deployment - Test 6

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,33 +1,43 @@
-import mongoose from 'mongoose';
-import AWS from 'aws-sdk';
+import mongoose from "mongoose";
 
-// Create an SSM or Secrets Manager client
-const ssm = new AWS.SSM();
+// Use this code snippet in your app.
+// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
 
-const getSecretValue = async (secretName) => {
-  try {
-    const data = await ssm.getParameter({
-      Name: secretName,
-      WithDecryption: true,  // Decrypt the secret value
-    }).promise();
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
 
-    return data.Parameter.Value;
-  } catch (err) {
-    console.error("Error fetching secret:", err);
-    throw err;
-  }
-};
+const secret_name = "MONGO_URI";
+
+const client = new SecretsManagerClient({
+  region: "us-east-1",
+});
+
+let response;
+
+try {
+  response = await client.send(
+    new GetSecretValueCommand({
+      SecretId: secret_name,
+      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+    })
+  );
+} catch (error) {
+  // For a list of exceptions thrown, see
+  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+  throw error;
+}
+
+const secret = response.SecretString;
 
 export const connectDB = async () => {
   try {
-    // Fetch the Mongo URI from AWS SSM
-    const mongoUri = await getSecretValue('/amplify/d2wr6azxcouomt/main/MONGO_URI');
-    
-    // Connect to MongoDB using the URI
-    const conn = await mongoose.connect(mongoUri);
+    const conn = await mongoose.connect(secret);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(1);  // Exit with failure code
+    process.exit(1); // process code 1 means exit with failure, 0 means exit with success
   }
-};
+}
